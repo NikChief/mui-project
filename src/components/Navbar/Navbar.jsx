50f980637985e89ade7cdfd81b370ccdd9a7fa9a
NikChief <niki3000-90@mail.ru>
2022-06-02 20:03:25 +0500

    navbar styles ready

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
-import { AppBar, Badge, Box, InputBase, styled, Toolbar, Typography } from '@mui/material';
+import { AppBar, Avatar, Badge, Box, InputBase, Menu, MenuItem, styled, Toolbar, Typography } from '@mui/material';
 import TapasIcon from '@mui/icons-material/Tapas';
-import React from 'react';
-import { Mail } from '@mui/icons-material';
+import React, { useState } from 'react';
+import { Mail, Notifications } from '@mui/icons-material';
 
 const StyledToolBar = styled(Toolbar)({
   display: 'flex',
@@ -16,9 +16,25 @@ const Search = styled("div")(({ theme }) => ({
 }))
 
 const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: 'center',
+  gap: '20px',
+  [theme.breakpoints.up('sm')]:{
+    display: 'flex'
+  }
+}))
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: '10px',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none'
+  }
 }))
 
 function Navbar(props) {
+  const [open, setOpen] = useState(false)
   return (
     <AppBar position='sticky'>
       <StyledToolBar>
@@ -36,13 +52,47 @@ function Navbar(props) {
             sm: 'none'
           }
         }} />
-        <Search><InputBase placeholder='Search somebody or something...'/></Search>
-        <Icons><Badge badgeContent={4} color="error">
-          <Mail />
-        </Badge>
-
+        <Search><InputBase placeholder='Search...' /></Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar alt="Nik" src="avatar.jpg" sx={{
+            width: '30px',
+            height: '30px'
+          }} onClick={()=>setOpen(true)}/>
         </Icons>
+        <UserBox onClick={() => setOpen(true)}>
+          <Avatar alt="Nik" src="avatar.jpg" sx={{
+            width: '30px',
+            height: '30px'
+          }} />
+          <Typography variant='span'>
+            Nik
+          </Typography>
+        </UserBox>
       </StyledToolBar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(()=>setOpen(false))}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 }
